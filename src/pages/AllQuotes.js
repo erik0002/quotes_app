e126405e674f8212079ec0808517df3c62d1e97b
Yeliazar <erik.ruzhakov@mail.ru>
2022-10-04 00:02:35 +0300

    Outputting Data on the "Details" Page

--- src/pages/AllQuotes.js
+++ src/pages/AllQuotes.js
@@ -3,7 +3,7 @@ import QuoteList from "../components/quotes/QuoteList";
 const DUMMY_QUOTES = [
     {id: 'q1', author: 'Yel', text: 'Learning React is Fun!'},
     {id: 'q2', author: 'Yeliazar', text: 'Learning React is Great!'},
-]
+];
 
 const AllQuotes = () => {
     return <QuoteList quotes={DUMMY_QUOTES}/>
